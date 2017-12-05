97662fdf358ee3973b5248c03c56d16bd2f1152f
MRR\Rizki <mr.rmdhn6@gmail.com>
2017-12-06 01:04:12 +0700

    new js databse

--- application/libraries/Sourcespage.php
+++ application/libraries/Sourcespage.php
@@ -2,7 +2,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 class Sourcespage 
 {
-    function sourceslogin()
+    function sourceslogincss()
    {
      $ret = '
      <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
@@ -30,6 +30,23 @@ class Sourcespage
      return $ret;
 
    } 
+   function sourcesloginjs()
+   {
+       $ret = '<script src="'.base_url().'appsources/bower_components/jquery/dist/jquery.min.js"></script>
+       <!-- Bootstrap 3.3.7 -->
+       <script src="'.base_url().'appsources/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+       <!-- iCheck -->
+       <script src="'.base_url().'appsources/plugins/iCheck/icheck.min.js"></script>
+       <script>
+         $(function () {
+           $("input").iCheck({
+             checkboxClass: "icheckbox_square-blue",
+             radioClass: "iradio_square-blue",
+             increaseArea: "20%" // optional
+           });
+         });
+       </script>';
+       return $ret;
+   }
 }
-
 ?>
--- application/modules/Login/views/auth.php
+++ application/modules/Login/views/auth.php
@@ -5,36 +5,29 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>AdminLTE 2 | Log in</title>
   <!-- Tell the browser to be responsive to screen width -->
-  <?=$this->sourcespage->sourceslogin();
+  <?=$this->sourcespage->sourceslogincss();
   ?>
 </head>
 <body class="hold-transition login-page">
 <div class="login-box">
   <div class="login-logo">
-    <a href="<?=base_url()?>appsources/index2.html"><b>Admin</b>VMD</a>
+    <img width="200px" src="<?=base_url()?>appsources/dist/pp.png"/>
   </div>
   <!-- /.login-logo -->
   <div class="login-box-body">
     <p class="login-box-msg">Sign in to start your session</p>
 
-    <form action="<?=base_url()?>appsources/index2.html" method="post">
+    <form id='form_login' method="post">
       <div class="form-group has-feedback">
-        <input type="email" class="form-control" placeholder="Email">
+        <input type="text" name="username" class="form-control" placeholder="Username">
         <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
       </div>
       <div class="form-group has-feedback">
-        <input type="password" class="form-control" placeholder="Password">
+        <input type="password" name="password" class="form-control" placeholder="Password">
         <span class="glyphicon glyphicon-lock form-control-feedback"></span>
       </div>
       <div class="row">
-        <div class="col-xs-8">
-          <div class="checkbox icheck">
-            <label>
-              <input type="checkbox"> Remember Me
-            </label>
-          </div>
-        </div>
-        <!-- /.col -->
+         <!-- /.col -->
         <div class="col-xs-4">
           <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
         </div>
@@ -42,37 +35,13 @@
       </div>
     </form>
 
-    <div class="social-auth-links text-center">
-      <p>- OR -</p>
-      <a href="#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i> Sign in using
-        Facebook</a>
-      <a href="#" class="btn btn-block btn-social btn-google btn-flat"><i class="fa fa-google-plus"></i> Sign in using
-        Google+</a>
-    </div>
-    <!-- /.social-auth-links -->
-
-    <a href="#">I forgot my password</a><br>
-    <a href="register.html" class="text-center">Register a new membership</a>
-
   </div>
   <!-- /.login-box-body -->
 </div>
 <!-- /.login-box -->
 
 <!-- jQuery 3 -->
-<script src="<?=base_url()?>appsources/bower_components/jquery/dist/jquery.min.js"></script>
-<!-- Bootstrap 3.3.7 -->
-<script src="<?=base_url()?>appsources/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-<!-- iCheck -->
-<script src="<?=base_url()?>appsources/plugins/iCheck/icheck.min.js"></script>
-<script>
-  $(function () {
-    $('input').iCheck({
-      checkboxClass: 'icheckbox_square-blue',
-      radioClass: 'iradio_square-blue',
-      increaseArea: '20%' // optional
-    });
-  });
-</script>
+<?=$this->sourcespage->sourcesloginjs();
+  ?>
 </body>
 </html>
