34a86a91d1bb5f1abc4406f8412d137702c98c02
DESKTOP-6U8JP9R\Ricky <rickyhermawan03@gmail.com>
2017-12-08 02:18:36 +0700

    ubah config

--- application/libraries/Layout.php
+++ application/libraries/Layout.php
@@ -8,5 +8,20 @@ class Layout
         $data["pages"] = $view;
         $CI->load->view("templates/content",$data);
     }
+
+    function sidemenu()
+    {
+        $CI =&get_instance();
+        $sql="select * from web_admin_menu where is_parent = '0'";
+        $query = $CI->db->query($sql);
+        $ret='<ul class="sidebar-menu" data-widget="tree">';
+        if ($query->num_rows()>0){
+            foreach($query->result() as $row){
+                $ret .='<li><a href="'.base_url().$row->url.'"><i class="fa fa-circle-o"></i>'.$row->nama_menu.'</a></li>';
+            }
+        }
+        $ret .='</ul>';
+        return $ret;
+    }
 }
 ?>
