a0ac5a6d070d869330b2e757c92af4c65721622e
MRR\Rizki <mr.rmdhn6@gmail.com>
2017-12-13 04:27:36 +0700

    menu team

---
+++ application/modules/Team/views/index.php
@@ -0,0 +1,37 @@
+<section class="content">
+      <div class="row">
+        <!-- left column -->
+        <div class="col-md-6">
+          <!-- general form elements -->
+      <div class="box box-primary">
+            <div class="box-header with-border">
+              <h2>Team</h2>
+            </div>
+            <!-- /.box-header -->
+            <!-- form start -->
+            <form id="formteam">
+              <div class="box-body">
+                <div class="form-group">
+                  <label for="exampleInputEmail1">Nama Angggota</label>
+                  <input name="nama_anggota" type="text" class="form-control" id="exampleInputEmail1" placeholder="Masukkan Nama">
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputPassword1">Divisi Anggota</label>
+                  <input name="nama_divisi" type="text" class="form-control" id="exampleInputPassword1" placeholder="Masukkan Divisi">
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputPassword1">Jabatan</label>
+                  <input name="nama_jabatan" type="text" class="form-control" id="exampleInputPassword1" placeholder="Masukkan Jabatan">
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputFile">File input</label>
+                  <input type="file" id="exampleInputFile">
+                </div>
+              <!-- /.box-body -->
+
+              <div class="box-footer">
+                <button type="submit" class="btn btn-primary">Submit</button>
+              </div>
+            </form>
+          </div>
+          </section>
