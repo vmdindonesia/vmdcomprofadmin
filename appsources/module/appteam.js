1e26eef1ed40a9b1c036530392acdd53f000b0ea
MRR\Rizki <mr.rmdhn6@gmail.com>
2017-12-20 03:41:15 +0700

    delete + edit

--- appsources/module/appteam.js
+++ appsources/module/appteam.js
@@ -4,7 +4,7 @@ var toUrl 	= getUrl .protocol + "//" + getUrl.host + "/" + getUrl.pathname.split
 $('#formteam').submit(function(event){
 
     event.preventDefault();
-    var form = $('#formteam');
+    var form = new FormData(this);
 
 // alert(toUrl);
 // return;
@@ -12,12 +12,89 @@ $('#formteam').submit(function(event){
 
         type:'POST',
         url:toUrl+'/team/saveteam',
-        data:form.serialize(),
+        data:form,
+        cache: false,
+        contentType: false,
+        processData: false,
         success:function(data){
-            alert(data);
-            return;
             if (data=='sukses'){
                 window.location.href=toUrl+"/Dashboard";
+            }
+            else{
+                $("#password").val("");
+                alert("Password atau username salah");
+
+            }
+
+        },error: function(xhr, ajaxOptions, thrownError){            
+			alert(xhr.responseText);
+			$("#loginbutton").html('<button class="btn waves-effect waves-light col s12">Login</button>');
+		}
+    })
+})
+
+function delete_team(team_id){
+    var getUrl 	= window.location;
+    var toUrl 	= getUrl .protocol + "//" + getUrl.host + "/" + getUrl.pathname.split('/')[1];
+    $.ajax({
+        
+                type:'POST',
+                url:toUrl+'/team/deleteteam',
+                data:{team_id:team_id},
+                success:function(data){
+                    if(data="sukses"){
+                        window.location.reload();
+                    }else{
+                        alert("gagal delete");
+                    }
+
+                },error: function(xhr, ajaxOptions, thrownError){            
+                    alert(xhr.responseText);
+                }
+            })
+}
+
+function edit_team(team_id){
+    var getUrl 	= window.location;
+    var toUrl 	= getUrl .protocol + "//" + getUrl.host + "/" + getUrl.pathname.split('/')[1];
+    $.ajax({
+        
+                type:'POST',
+                url:toUrl+'/team/deleteteam',
+                data:{team_id:team_id},
+                success:function(data){
+                    if(data="sukses"){
+                        window.location.reload();
+                    }else{
+                        alert("gagal delete");
+                    }
+
+                },error: function(xhr, ajaxOptions, thrownError){            
+                    alert(xhr.responseText);
+                }
+            })
+}
+document.addEventListener('contextmenu', event => event.preventDefault());
+var getUrl 	= window.location;
+var toUrl 	= getUrl .protocol + "//" + getUrl.host + "/" + getUrl.pathname.split('/')[1];
+$('#formeditteam').submit(function(event){
+
+    event.preventDefault();
+    var form = new FormData(this);
+
+// alert(toUrl);
+// return;
+    $.ajax({
+
+        type:'POST',
+        url:toUrl+'/team/save_edit_team',
+        data:form,
+        cache: false,
+        contentType: false,
+        processData: false,
+        success:function(data){
+            if (data=='sukses'){
+                window.location.href=toUrl+"/team";
             }
             else{
                 $("#password").val("");
